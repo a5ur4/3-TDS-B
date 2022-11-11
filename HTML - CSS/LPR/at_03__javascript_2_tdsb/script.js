dc087388fac2e2c4f2707e5e2f25547c346b4cc1
_a5ur4 <113993134+a5ur4@users.noreply.github.com>
2022-11-11 14:47:39 -0300

    Update script.js

--- HTML - CSS/LPR/at_03__javascript_2_tdsb/script.js
+++ HTML - CSS/LPR/at_03__javascript_2_tdsb/script.js
@@ -1,9 +1,24 @@
 function calcularMedia(){
     var nota1 = parseInt(document.getElementById("nota1").value)
+    if(isNaN(nota1)){
+        alert("Valor invalido, digite todos os termos")
+    }
     var nota2 = parseInt(document.getElementById("nota2").value)
+    if(isNaN(nota2)){
+        alert("Valor invalido, digite todos os termos")
+    }
     var nota3 = parseInt(document.getElementById("nota3").value)
+    if(isNaN(nota3)){
+        alert("Valor invalido, digite todos os termos")
+    }
     var nota4 = parseInt(document.getElementById("nota4").value)
+    if(isNaN(nota4)){
+        alert("Valor invalido, digite todos os termos")
+    }
     var media = nota1 + nota2 + nota3 + nota4
     var media_final = media / 4
+    if (isNaN(media_final)){
+        media_final = "?"
+    }
     document.getElementById("media").innerText = "MÉDIA = " + media_final
 } 
